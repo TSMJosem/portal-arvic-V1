92c4b5cfd8e185ca721dadebacf1024e364b17a0
TSMJosem <receptorx89@gmail.com>
2025-11-01 17:14:43 -0600

    Commit

--- api/index.js
+++ api/index.js
@@ -6,7 +6,15 @@ require('dotenv').config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
+    'https://tsmjosem.github.io',
+    'https://portalarvic-8fovmmmwa-josems-projects.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
--- js/databaseMongoDB.js
+++ js/databaseMongoDB.js
@@ -9,7 +9,7 @@ class PortalDatabase {
         // URL del API - detecta automáticamente si es producción o desarrollo
         this.API_URL = window.location.hostname === 'localhost' 
             ? 'http://localhost:3000/api' 
-            : 'https://portal-arvic-api.vercel.app/api';
+            : 'https://portalarvic-8fovmmmwa-josems-projects.vercel.app/';
         
         this.token = localStorage.getItem('arvic_token') || null;
         this.prefix = 'arvic_';
